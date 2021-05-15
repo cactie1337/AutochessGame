d7611342759c46481f0656904e4629d58306662c
beseqel <55451193+beseqel@users.noreply.github.com>
2021-05-16 00:58:42 +0300

    Menu ir volume
    Padariau sfx ir music sliderius, abu veikia. kiti meniu mygtukai irgi pilnai veikia. Padariau skybox'ą. Kur "Autochess" reikia pavadinimą sugalvot. Transitioną tarp scenu čj ryt pabandysiu padaryt, ir greičiausiai pakeisiu tuos medžius į kažką.

---
+++ Roguelike, autochess/Assets/Scenes/Scripts/MenuScripts/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void quitButton()
+    {
+        Application.Quit();
+    }
+    public void playButton()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Roguelike, autochess/Assets/Scenes/Scripts/MenuScripts/SettingsMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioMixer audioMixer;
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+    }
+}
