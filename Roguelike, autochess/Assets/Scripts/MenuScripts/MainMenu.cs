54268d4c2da51776518c8fa738db6aaa4ad63aaf
beseqel <55451193+beseqel@users.noreply.github.com>
2021-05-23 21:28:44 +0300

    :(

--- Roguelike, autochess/Assets/Scripts/MenuScripts/MainMenu.cs
+++ Roguelike, autochess/Assets/Scripts/MenuScripts/MainMenu.cs
@@ -6,15 +6,12 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public sc SceneChanger;
-    public GameManager gm;
-    public bool clicked = false;
     public void quitButton()
     {
         Application.Quit();
     }
     public void playButton()
     {
-        clicked = true;
         SceneChanger = FindObjectOfType(typeof(sc)) as sc;
         SceneChanger.ToLevel(SceneManager.GetActiveScene().buildIndex + 1);
         //SceneManager.LoadScene("Mountain");
